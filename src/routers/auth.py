d5c8cbd1c0310f4861a7a039d5b5607e145c9e73
Kunal Mallick <kunalmallicks3@gmail.com>
2024-07-01 22:09:27 +0530

    fixing bug

--- src/routers/auth.py
+++ src/routers/auth.py
@@ -1,11 +1,14 @@
 from fastapi import APIRouter, Request
 from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from src.models.model import model
 
 router = APIRouter()
 
+router.mount("/static", StaticFiles(directory="src/static"), name="static")
+
 templates = Jinja2Templates(directory="templates")
 @router.get("/",response_class= HTMLResponse)
-def home(request: Request):
-    return templates.TemplateResponse("index.html",{"request" : request})
+async def home(request: Request):
+    return templates.TemplateResponse(request=request, name="item.html")
